270b250176463ea35eda903584723cc1d7827d18
Savannah Drake <savannah.drake@emory.edu>
2026-02-15 15:12:33 -0500

    Remove Vite demo code adding App details

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,31 +4,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <h1>SprintWheel</h1>
+      <p>Welcome to SprintWheel, your agile project management tool designed to streamline your workflow and enhance team collaboration. With SprintWheel, you can easily plan, track, and manage your sprints, ensuring that your projects stay on track and your team stays productive.</p>
+    </main>
+  
   )
 }
 
