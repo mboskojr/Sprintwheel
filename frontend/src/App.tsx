100f94874be7a097d1e0e2effec6d6903c25a416
laurengrove <laurengrove2023@gmail.com>
2026-02-15 22:40:48 -0500

    added login/sign up headers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,10 @@ function App() {
     <main>
       <h1>SprintWheel</h1>
       <p>Welcome to SprintWheel, your agile project management tool designed to streamline your workflow and enhance team collaboration. With SprintWheel, you can easily plan, track, and manage your sprints, ensuring that your projects stay on track and your team stays productive.</p>
+      <h2>Login</h2>
+
+      <h2>Sign Up</h2>
+    
     </main>
   
   )
