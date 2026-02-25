7661e54e5f2e0e0791a4ff74a1ebd5a04a7fa96f
laurengrove <laurengrove2023@gmail.com>
2026-02-24 20:02:43 -0500

    added a product owner page and route in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
 import {Routes, Route, Navigate} from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import DashboardPage from "./pages/DashboardPage";
+import ProductOwnerPage from "./pages/ProductOwnerPage";
+import type { JSX } from "react/jsx-dev-runtime"; //added quick fix
 
 function App(): JSX.Element {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/" element={<Navigate to="/product-owner" />} />
+      {/* <Route path="/" element={<Navigate to="/login" />} /> */}
       <Route path="/login" element={<LoginPage />} />
       <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/product-owner" element={<ProductOwnerPage />} />
+        <Route path="*" element={<Navigate to="/product-owner" />} />
     </Routes>
   );
 }
---
+++ frontend/src/pages/ProductOwnerPage.tsx
@@ -0,0 +1,10 @@
+import type { CSSProperties, JSX} from "react";
+
+export default function ProductOwnerPage(): JSX.Element {
+    return (
+        <div style={{ color: "black", padding: 40 }}>
+            <h1>Product Owner Page</h1>
+            <p>new page!</p>
+        </div>
+    );
+}
