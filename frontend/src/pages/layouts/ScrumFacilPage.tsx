6bce60a8cfa6302601e6efeaff990ca4b15ff2c8
laurengrove <laurengrove2023@gmail.com>
2026-02-27 12:26:52 -0500

    changed text color

--- frontend/src/pages/layouts/ScrumFacilPage.tsx
+++ frontend/src/pages/layouts/ScrumFacilPage.tsx
@@ -2,7 +2,7 @@ import type { CSSProperties, JSX} from "react";
 
 export default function ScrumFacilPage(): JSX.Element {
     return (
-        <div style={{ color: "white", padding: 40 }}>
+        <div style={{ color: "black", padding: 40 }}>
             <h1>Scrum Facilitator Page</h1>
             <p>new page!</p>
         </div>
