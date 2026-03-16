afe4f661078805f08d35f0fbf128c3ebecbc2b2e
laurengrove <laurengrove2023@gmail.com>
2026-03-16 17:23:12 -0400

    added static sprint burndown chart with mock data

--- frontend/src/pages/ProgressPage.tsx
+++ frontend/src/pages/ProgressPage.tsx
@@ -1,17 +1,18 @@
 import type{ CSSProperties, JSX } from "react";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from "recharts";
-
+//import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from "recharts";
+import SprintBurndownChart from "../components/SprintBurndownChart";
 
 export default function ProgressPage(): JSX.Element {
 
     const containerStyle: CSSProperties = { color: "black", padding: 40 };
 
     // data to hold space for test graph
-    const testData = [
+    {/*const testData = [
         { date: "Day 1", remaining: 0, ideal: 0 },
         { date: "Day 2", remaining: 0, ideal: 0 },
         { date: "Day 3", remaining: 0, ideal: 0 },
     ];
+    */}
 
 
     return (
@@ -22,13 +23,20 @@ export default function ProgressPage(): JSX.Element {
 
             <img src="/sprint_burndown_chart.png" alt="Sprint Burndown Chart showing task completion over time" style={{ maxWidth: "100%", height: "auto", marginTop: 20 }} />
 
+            <h2>Sprint #BLANK Burndown Chart</h2>
+            <div style={{ padding: 40 }}>
+                <SprintBurndownChart />
+            </div>
+
+
             {/* Placeholder Recharts graph*/}
+            {/*
             <div style={{ marginTop: 20}}>
                 <LineChart
                 width={800}
                 height={350}
                 data={testData}
-                margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+                margin={{ top: 20, right: 30, left: 5, bottom: 10 }}
                 >
                     <CartesianGrid strokeDasharray="3 3" />
                     <XAxis dataKey="date" />
@@ -39,6 +47,9 @@ export default function ProgressPage(): JSX.Element {
                     <Line type="monotone" dataKey="ideal" stroke="#8884d8" strokeDasharray="5 5" name="Ideal" />
                 </LineChart>
             </div>
+            */}
+
+
         </div>
     );
 }
